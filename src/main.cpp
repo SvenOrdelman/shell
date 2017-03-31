86f0b7c9a7998a305ad83b7f4315d22701580b58
sven <sveno98@live.nl>
2017-03-31 14:27:21 +0200

    Commitje

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,29 @@
 std::string working_directory = "/";
 
 int main() {
-	std::cout << working_directory << " $ ";
-	std::cout.flush();
+	for(;;) {
+		std::cout << working_directory << " $ ";
+		std::cout.flush();
 
-	std::string line;
-	getline(std::cin, line);
+		std::string line;
+		getline(std::cin, line);
+		if(line == "exit"){
+			break;
+		}
 
-	// Create input stream, create lexer and use lexer to create stream of tokens
-	antlr4::ANTLRInputStream inputStream(line);
-	ShellGrammarLexer lexer(&inputStream);
-	antlr4::CommonTokenStream tokens(&lexer);
+		// Create input stream, create lexer and use lexer to create stream of tokens
+		antlr4::ANTLRInputStream inputStream(line);
+		ShellGrammarLexer lexer(&inputStream);
+		antlr4::CommonTokenStream tokens(&lexer);
 
-	// Create parser
-	ShellGrammarParser parser(&tokens);
-	antlr4::tree::ParseTree *parseTree = parser.command();
+		// Create parser
+		ShellGrammarParser parser(&tokens);
+		antlr4::tree::ParseTree *parseTree = parser.command();
 
-	// Then, visit your tree
-	// MyVisitor visitor;
-	// visitor.visit(parseTree);
+		// Then, visit your tree
+		// MyVisitor visitor;
+		// visitor.visit(parseTree);
+	}
 
 	return 0;
 }
