6aa7a40e67ac8bc33f1cf6e800cdbdd72904fc2e
sven <sveno98@live.nl>
2017-03-31 14:38:46 +0200

    Commitje

---
+++ src/PearlVisitor.cpp
@@ -0,0 +1,29 @@
+//
+// Created by sven on 31-3-17.
+//
+
+#include "PearlVisitor.h"
+
+antlrcpp::Any PearlVisitor::visitLine(ShellGrammarParser::LineContext *ctx) {
+    return ShellGrammarBaseVisitor::visitLine(ctx);
+}
+
+antlrcpp::Any PearlVisitor::visitCommand(ShellGrammarParser::CommandContext *ctx) {
+    return ShellGrammarBaseVisitor::visitCommand(ctx);
+}
+
+antlrcpp::Any PearlVisitor::visitProgram(ShellGrammarParser::ProgramContext *ctx) {
+    return ShellGrammarBaseVisitor::visitProgram(ctx);
+}
+
+antlrcpp::Any PearlVisitor::visitArgument(ShellGrammarParser::ArgumentContext *ctx) {
+    return ShellGrammarBaseVisitor::visitArgument(ctx);
+}
+
+antlrcpp::Any PearlVisitor::visitFlag(ShellGrammarParser::FlagContext *ctx) {
+    return ShellGrammarBaseVisitor::visitFlag(ctx);
+}
+
+antlrcpp::Any PearlVisitor::visitParameter(ShellGrammarParser::ParameterContext *ctx) {
+    return ShellGrammarBaseVisitor::visitParameter(ctx);
+}
---
+++ src/PearlVisitor.h
@@ -0,0 +1,29 @@
+//
+// Created by sven on 31-3-17.
+//
+
+#ifndef SHELL_PEARLVISITOR_H
+#define SHELL_PEARLVISITOR_H
+
+
+#include "../gen/ShellGrammarBaseVisitor.h"
+
+class PearlVisitor : public ShellGrammarBaseVisitor{
+
+public:
+    antlrcpp::Any visitLine(ShellGrammarParser::LineContext *ctx) override;
+
+    antlrcpp::Any visitCommand(ShellGrammarParser::CommandContext *ctx) override;
+
+    antlrcpp::Any visitProgram(ShellGrammarParser::ProgramContext *ctx) override;
+
+    antlrcpp::Any visitArgument(ShellGrammarParser::ArgumentContext *ctx) override;
+
+    antlrcpp::Any visitFlag(ShellGrammarParser::FlagContext *ctx) override;
+
+    antlrcpp::Any visitParameter(ShellGrammarParser::ParameterContext *ctx) override;
+
+};
+
+
+#endif //SHELL_PEARLVISITOR_H
