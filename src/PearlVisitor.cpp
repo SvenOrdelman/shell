de1aa24fc99d7e0be4d7068f9da8071dd9718bcb
jeroen <jeroensmienk@icloud.com>
2017-04-07 19:35:26 +0200

    try

--- src/PearlVisitor.cpp
+++ src/PearlVisitor.cpp
@@ -6,64 +6,68 @@
 #include <wait.h>
 #include "PearlVisitor.h"
 
-std::string bin = "/bin/";
+using namespace std;
 
-int file_descriptor;
+// program directory
+const string bin = "/bin/";
 
-std::string program;
-std::string parameter;
+string program;
+string parameter;
 
-std::list<std::string> parameters;
+vector<string> parameters;
 
-bool pipeline;
-int direction = 0;
-
-int change_working_directory(std::list<std::string>* list);
+int change_working_directory(vector<string>* list);
+void execute(vector<string>* params);
 
 antlrcpp::Any PearlVisitor::visitLine(ShellGrammarParser::LineContext *ctx) {
 
     // visit all commands one by one from the end to the front
-    std::vector<antlr4::tree::ParseTree*> children = ctx->children;
+    vector<antlr4::tree::ParseTree*> children = ctx->children;
 
     int c = (int) children.size();
-    for (std::vector<antlr4::tree::ParseTree*>::const_reverse_iterator i = children.crbegin(); i != children.crend(); ++i)
+    for (vector<antlr4::tree::ParseTree*>::const_reverse_iterator i = children.crbegin(); i != children.crend(); ++i)
     {
-        --c;
-
         // let the command gather the info
         visit(*i);
+
+        if (program == "cd") {
+            int result = change_working_directory(&parameters);
+            if (result == -1)
+            {
+                cerr << "Directory not found. " << endl;
+            }
+            break;
+        }
 
         int pid = fork();
         // first command
         if (pid == 0) {
-            // if not the last command
-//            if (c > 0) {
-//                int pid_c = fork();
-//
-//                if (pid_c == 0) {
-//                    // second command
-//                    continue;
-//                }
-//            }
+            for (;;) {
+                --c;
 
-            std::string path = bin + program;
+                // if not the last command
+                if (c > 0) {
 
-            // get the arguments
-            char *cargs[parameters.size() + 2];
-            int a = 0;
-            cargs[a] = (char *) path.c_str();
-            for (std::list<std::string>::const_iterator j = parameters.cbegin(); j != parameters.cend(); ++j)
-            {
-                ++a;
-                cargs[a] = (char *) (*j).c_str();
+                }
+
+                string path = bin + program;
+
+                // get the arguments
+                char *cargs[parameters.size() + 2];
+                int a = 0;
+                cargs[a] = (char *) path.c_str();
+                for (vector<string>::const_iterator j = parameters.cbegin(); j != parameters.cend(); ++j) {
+                    ++a;
+                    cargs[a] = (char *) (*j).c_str();
+                }
+                cargs[a + 1] = NULL;
+
+                // execute the program
+                execvp(cargs[0], cargs);
+                cerr << "No program '" << program << "' found." << endl;
+                exit(0);
+                // unreachable
             }
-            cargs[a + 1] = NULL;
-
-            // execute the program
-            execvp(cargs[0], cargs);
-            std::cerr << "No program '" << cargs[0] << "' found." << std::endl;
-            exit(0);
-            // unreachable
         }
         parameters.clear();
 
@@ -75,19 +79,21 @@ antlrcpp::Any PearlVisitor::visitLine(ShellGrammarParser::LineContext *ctx) {
     return true;
 }
 
+void execute(vector<string>* params) {
+    int pid = fork();
+
+
+
+    if (pid == 0) {
+        // second command
+        execute(params);
+    }
+}
+
 antlrcpp::Any PearlVisitor::visitCommand(ShellGrammarParser::CommandContext *ctx) {
     program = ctx->p->getText();
 
     antlrcpp::Any any = visitChildren(ctx);
-
-    if (program == "cd") {
-        int result = change_working_directory(&parameters);
-        if (result == -1)
-        {
-            std::cerr << "Directory not found. " << std::endl;
-        }
-        return any;
-    }
 
     return any;
 }
@@ -98,22 +104,20 @@ antlrcpp::Any PearlVisitor::visitExtra(ShellGrammarParser::ExtraContext *ctx) {
 }
 
 antlrcpp::Any PearlVisitor::visitInput(ShellGrammarParser::InputContext *ctx) {
-    std::cout << "Input?: " << parameter << std::endl;
-    direction = -1;
+    cout << "Input?: " << parameter << endl;
     return ShellGrammarBaseVisitor::visitInput(ctx);
 }
 
 antlrcpp::Any PearlVisitor::visitOutput(ShellGrammarParser::OutputContext *ctx) {
-    std::cout << "Output?: " << parameter << std::endl;
-    direction = 1;
+    cout << "Output?: " << parameter << endl;
     return ShellGrammarBaseVisitor::visitOutput(ctx);
 }
 
-int change_working_directory(std::list<std::string>* list) {
+int change_working_directory(vector<string>* ls) {
     int result = -1;
-    if (list->size() > 0) {
-        result = chdir(list->back().c_str());
-        list->pop_back();
+    if (ls->size() > 0) {
+        result = chdir(ls->back().c_str());
+        ls->pop_back();
     }
     return result;
 }
